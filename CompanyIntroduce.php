e7b1d68564c62ef4d6ef3325d819e69fe1d248a5
Minamo <feel2548899z@gmail.com>
2017-07-14 10:15:11 +0800

    data-base link problem fixed

--- CompanyIntroduce.php
+++ CompanyIntroduce.php
@@ -5,7 +5,7 @@ if($_SESSION['login']!="yes")
 mysql_connect("localhost","root","admin"); //連結伺服器
 mysql_select_db("company"); //選擇資料庫
 mysql_query("set names utf8"); //以utf-8讀取資料，讓資料可以讀取中文
-$data=mysql_query("select * from companyIntroduce"); //從contact資料庫中選擇所有的資料表
+$data=mysql_query("select * from companyintroduce"); //從contact資料庫中選擇所有的資料表
 ?>
 
 <!DOCTYPE html>
