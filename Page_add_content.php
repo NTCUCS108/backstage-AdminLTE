b6b722fd885c8bd96ba4824d2ecdbf1c62f7cf53
RecorderZero <ian03121997@gmail.com>
2017-07-24 14:44:36 +0800

    change outlooking of dropdownlist

--- Page_add_content.php
+++ Page_add_content.php
@@ -10,14 +10,17 @@
                             <div class="box-body pad">
                                 <form method="post">
 								頁面名稱：<input type='text' name='name'><?php if($error == 1) echo "有重複頁面名稱，請重新輸入";?>
-                                父頁面：<select name="parent">
-                                <option value="首頁" <?php if($_POST['parent'] == "首頁") echo "selected=selected";?>>首頁</option>
-                                <option value="公司簡介" <?php if($_POST['parent'] == "公司簡介") echo "selected=selected";?>>公司簡介</option>
-                                <option value="產品資訊" <?php if($_POST['parent'] == "產品資訊") echo "selected=selected";?>>產品資訊</option>
-                                <option value="連絡方式" <?php if($_POST['parent'] == "連絡方式") echo "selected=selected";?>>連絡方式</option>
-                                <option value="留言板" <?php if($_POST['parent'] == "留言板") echo "selected=selected";?>>留言板</option>
-                                <option value="更多" <?php if($_POST['parent'] == "更多") echo "selected=selected";?>>更多</option>
-                                <option value="測試" <?php if($_POST['parent'] == "測試") echo "selected=selected";?>>測試</option>
+                                父頁面：<select name="parent" type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown">
+                                <span class="fa fa-caret-down"></span>
+                                <ul class="dropdown-menu">
+                                  <option value="首頁" <?php if($rs['parent'] == "首頁") echo "selected=selected";?>>首頁</option>
+                                  <option value="公司簡介" <?php if($rs['parent'] == "公司簡介") echo "selected=selected";?>>公司簡介</option>
+                                  <option value="產品資訊" <?php if($rs['parent'] == "產品資訊") echo "selected=selected";?>>產品資訊</option>
+                                  <option value="連絡方式" <?php if($rs['parent'] == "連絡方式") echo "selected=selected";?>>連絡方式</option>
+                                  <option value="留言板" <?php if($rs['parent'] == "留言板") echo "selected=selected";?>>留言板</option>
+                                  <option value="更多" <?php if($rs['parent'] == "更多") echo "selected=selected";?>>更多</option>
+                                  <option value="測試" <?php if($rs['parent'] == "測試") echo "selected=selected";?>>測試</option>
+                                </ul>
                                 </select>
                                     <textarea id="editor1" name="editor1" rows="10" cols="80">
                                     <?php
--- Page_edit_content.php
+++ Page_edit_content.php
@@ -9,17 +9,18 @@
                             <!-- /.box-header -->
                             <div class="box-body pad">
                                 <form method="post">
-                                <?php $tmp=mysql_query("select * from page_data where name = '$rs[name]'");
-                                        $display = mysql_fetch_assoc($tmp);?>
 								頁面名稱：<input type='text' name='name' value=<?php echo "$rs[name]";?>><?php if($error == 1) echo "有重複頁面名稱，請重新輸入";?>
-                                父頁面：<select name="parent">
-                                <option value="首頁" <?php if($rs['parent'] == "首頁") echo "selected=selected";?>>首頁</option>
-                                <option value="公司簡介" <?php if($rs['parent'] == "公司簡介") echo "selected=selected";?>>公司簡介</option>
-                                <option value="產品資訊" <?php if($rs['parent'] == "產品資訊") echo "selected=selected";?>>產品資訊</option>
-                                <option value="連絡方式" <?php if($rs['parent'] == "連絡方式") echo "selected=selected";?>>連絡方式</option>
-                                <option value="留言板" <?php if($rs['parent'] == "留言板") echo "selected=selected";?>>留言板</option>
-                                <option value="更多" <?php if($rs['parent'] == "更多") echo "selected=selected";?>>更多</option>
-                                <option value="測試" <?php if($rs['parent'] == "測試") echo "selected=selected";?>>測試</option>
+                                父頁面：<select name="parent" type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown">
+                                <span class="fa fa-caret-down"></span>
+                                <ul class="dropdown-menu">
+                                  <option value="首頁" <?php if($rs['parent'] == "首頁") echo "selected=selected";?>>首頁</option>
+                                  <option value="公司簡介" <?php if($rs['parent'] == "公司簡介") echo "selected=selected";?>>公司簡介</option>
+                                  <option value="產品資訊" <?php if($rs['parent'] == "產品資訊") echo "selected=selected";?>>產品資訊</option>
+                                  <option value="連絡方式" <?php if($rs['parent'] == "連絡方式") echo "selected=selected";?>>連絡方式</option>
+                                  <option value="留言板" <?php if($rs['parent'] == "留言板") echo "selected=selected";?>>留言板</option>
+                                  <option value="更多" <?php if($rs['parent'] == "更多") echo "selected=selected";?>>更多</option>
+                                  <option value="測試" <?php if($rs['parent'] == "測試") echo "selected=selected";?>>測試</option>
+                                </ul>
                                 </select>
                                     <textarea id="editor1" name="editor1" rows="10" cols="80">
                                     <?php
@@ -27,7 +28,8 @@
 										echo "$_POST[editor1]";
 									else
 									{
-										
+										$tmp=mysql_query("select * from page_data where name = '$rs[name]'");
+                                        $display = mysql_fetch_assoc($tmp);
 										echo "$display[content]";
 									}
                                     ?>
