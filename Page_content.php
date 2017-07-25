2310c8def0061865006fd26ea68ddc475a4bccbe
Minamo <feel2548899z@gmail.com>
2017-07-25 10:41:16 +0800

    Page_content ok

--- Page_content.php
+++ Page_content.php
@@ -1,13 +1,20 @@
-<br><br>
+<br>
+<section class="content">
+<div class="row">
+<div class="col-md-offset-2 col-md-8">
+<div class="box">
+			<div class="box-header">
 				<h1 align='center'>管理頁面板</h1><br>
-				<button onclick="location.href='./Page_add.php'">新增</button>
+			</div>
+			<div class="box-body">
+				<button class="btn btn-success" onclick="location.href='./Page_add.php'">新增</button>
 				<form name='delete_page' method='post'>
-				<input type='submit' value='刪除勾選頁面'>
+				<input class="btn btn-warning" type='submit' value='刪除勾選頁面'>
 				<?php
 				for($i=1;$i<=mysql_num_rows($data);$i++){
 					$rs = mysql_fetch_assoc($data);
 				?>
-				<table align="center" width="20%" border="1">
+				<table class="table table-bordered" align="center" width="20%" border="1">
 					<tr>
 						<td width="15%"><input type='checkbox' name='delete[]' value='<?php echo "$rs[name]";?>'></td>
 						<td width="15%"><?php echo "$rs[parent]";?></td>
@@ -18,3 +25,8 @@
 				}
 				?>
 				</form>
+			</div>
+</div>
+</div>
+</div>
+</section>
