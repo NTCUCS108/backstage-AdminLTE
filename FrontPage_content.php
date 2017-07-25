382971bc27b2800b0e4395fae99647dadcc83a1e
RecorderZero <ian03121997@gmail.com>
2017-07-25 14:45:08 +0800

    frontpage editor start

--- FrontPage_content.php
+++ FrontPage_content.php
@@ -1,21 +1,32 @@
 <br>
 <div class="row">
-<div class="col-md-10">
+<div class="col-md-3">
 <div class="box">
         <div class="box-header">
                 <a href="Carousel_edit.php">
                     <button type="link" pull-right class="btn btn-primary">投影片編輯</button>
                 </a>
-            
-            <br><br>
-
-                	<a href="FrontPage_edit.php">
-                    	<button type="link" pull-right class="btn btn-primary">編輯</button>
-                	</a>
+                <br><br>
+                <a href="#">
+                    <button type="link" pull-right class="btn btn-primary">圈圈編輯</button>
+                </a>
+                <br><br>
+                <a href="#">
+                    <button type="link" pull-right class="btn btn-primary">列列編輯</button>
+                </a>
+        </div>
+</div>
+</div>
+<div class="col-md-7">
+<div class="box">
+        <div class="box-header">
+                    <a href="FrontPage_edit.php">
+                        <button type="link" pull-right class="btn btn-primary">編輯</button>
+                    </a>
             
         </div>
         <div class="box-body">
-           	~~~顯示主頁資料~~~
+            ~~~顯示主頁資料~~~
         </div>
 </div>
 </div>
