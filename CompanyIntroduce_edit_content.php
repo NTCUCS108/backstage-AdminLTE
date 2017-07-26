95f333978a26e5cb9c9100f1ca64c9878988397a
Minamo <feel2548899z@gmail.com>
2017-07-26 17:49:16 +0800

    editor plugin

--- CompanyIntroduce_edit_content.php
+++ CompanyIntroduce_edit_content.php
@@ -9,15 +9,23 @@
                             <!-- /.box-header -->
                             <div class="box-body pad">
                                 <form method="post">
+                                    <!--
                                     <textarea id="editor1" name="editor1" rows="10" cols="80">
+                                    -->
+                                    <textarea cols="10" id="editor2" name="editor2" rows="10" >
+
                                     <?php
                                     echo "$rs[0]";
                                     ?>
-
+                                    
                                     <script type="text/javascript">
                                         var content = Document.getElementById('editor1').value;
                                     </script>
+                                
                                     </textarea>
+
+
+
 
                                     <div class="row">
                                     <div class="col-md-8 col-md-offset-2">
